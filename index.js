1fc3e420b1d30269425b5a5731a6382f4d3387c4
AnneLund <89774650+AnneLund@users.noreply.github.com>
2022-05-03 09:02:14 +0200

    done

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-import http from 'http';
 import express from 'express';
-
-//Jeg opretter en server på port 4000:
-
-http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('Hello World!');
-    response.end();
-}).listen(4000)
 
 //Jeg deklarerer en variabel der påkalder serveren fra express
 const app = express();
